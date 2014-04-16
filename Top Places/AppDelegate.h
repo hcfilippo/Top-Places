cdeac5e8912e36a6c399610340e44794c42aa274
hcfilippo <805998420@qq.com>
2014-04-16 15:24:55 +0800

    Initial Commit

---
+++ Top Places/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Top Places
+//
+//  Created by hcfilippo on 14-4-16.
+//  Copyright (c) 2014年 hcfilippo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Top Places/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Top Places
+//
+//  Created by hcfilippo on 14-4-16.
+//  Copyright (c) 2014年 hcfilippo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
